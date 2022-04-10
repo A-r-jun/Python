793fffb16f4f72b18b0cb10b67b5c89cccc6733b
Arjun <Arjungodha@gmail.com>
2022-04-11 00:20:58 +0530

    Create Basic_Name_Age.py

---
+++ Basic_Name_Age.py
@@ -0,0 +1,9 @@
+# This program says Hello and ask for Name
+
+print('Hello World!')
+print('What is your name?') # ask for name
+name= input()
+print('It is good to meet you, '+name)
+print('What is your age?') # ask for age
+age=input()
+print('Oh God, you are old')
